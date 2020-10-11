f04111b332ecdc4873911219ba118e73ac029b1b
Jess Yoder <jessica.m.yoder@gmail.com>
2020-10-10 23:42:53 -0400

    Working on createFeatures(), not getting anywhere yet

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,13 +1,17 @@
+
+
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 d3.json(url, function(data){
-    console.log(data.features)
+  console.log(data.features)
+  createFeatures(data)
+});
+    // console.log(data.features)
     // var geometry = data.features[0].geometry;
     // console.log(geometry)
     // var properties = data.features[0].properties;
     // console.log(properties)
-    // createFeatures()
-})
+
 
 function createMap(earthquakes) {
   
@@ -19,17 +23,17 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+  var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     maxZoom: 18,
-    id: "streets-v11",
+    id: "satellite-v9",
     accessToken: API_KEY
   })
 
   // Create a baseMaps object to hold the lightmap layer
   var baseMaps = {
     "Light Map": lightmap,
-    "Street Map": streetmap
+    "Satellite": satellitemap
   };
 
   // Create an overlayMaps object to hold the earthquakes layer
@@ -38,9 +42,9 @@ function createMap(earthquakes) {
   }
 
   // Create the map object with options
-  var myMap = L.circle("mapid", {
+  var myMap = L.map("mapid", {
     center: [35.546, -77.0525],
-    zoom: 4,
+    zoom: 3,
     layers: [lightmap, earthquakes]
   });
 
@@ -48,31 +52,3 @@ function createMap(earthquakes) {
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
-}
-
-
-function createFeatures(response) {
-  
-  // Pull the "stations" property off of response.data
-  var features = response.features;
-
-  // Initialize an array to hold bike markers
-  var locations = [];
-
-  // Loop through the stations array
-  for (var index = 0; index < features.length; index++) {
-    var feature = features[index];
-
-    // For each station, create a marker and bind a popup with the station's name
-    var location = L.marker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]])
-      .bindPopup("<h3>" + feature.properties.place + "<h3><h3>Magnitude: " + feature.properties.mag + "</h3>");
-
-    // Add the marker to the bikeMarkers array
-    locations.push(location);
-  }
-
-  // Create a layer group made from the bike markers array, pass it into the createMap function
-  createMap(L.layerGroup(locations));
-}
-
-d3.json(url, createFeatures)
