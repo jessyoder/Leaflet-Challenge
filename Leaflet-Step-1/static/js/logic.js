47b9624d1141ba2d99201a77f4b066c04bdb8495
Jess Yoder <jessica.m.yoder@gmail.com>
2020-10-12 16:04:42 -0400

    Adding markers, then turning markers to circles

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,20 +1,49 @@
-
-
 var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-d3.json(url, function(data){
+d3.json(url, function(data) { 
   console.log(data.features)
-  createFeatures(data)
+  createFeatures(data.features); 
 });
-    // console.log(data.features)
-    // var geometry = data.features[0].geometry;
-    // console.log(geometry)
-    // var properties = data.features[0].properties;
-    // console.log(properties)
+
+function getColor(d) {
+  return d > 1000 ? '#800026' :
+         d > 500  ? '#BD0026' :
+         d > 200  ? '#E31A1C' :
+         d > 100  ? '#FC4E2A' :
+         d > 50   ? '#FD8D3C' :
+         d > 20   ? '#FEB24C' :
+         d > 10   ? '#FED976' :
+                    '#FFEDA0';
+};
+
+function markerSize(radius) {
+  return (radius);
+}
+
+function createFeatures(earthquakeData) {
+  var earthquakes = L.geoJSON(earthquakeData, {
+  // Define a function we want to run once for each feature in the features array
+  // Give each feature a popup describing the place and time of the earthquake
+  onEachFeature : function (feature, layer) {
+    layer.bindPopup('<h4>Location: ' + feature.properties.place + '</h4><hr><h4>Magnitude: ' + feature.properties.mag + '</h4>')
+    },
+    pointToLayer: function (feature, latlng) {
+      return new L.circle(latlng,
+        {radius: markerSize(feature.properties.mag),
+        fillColor: getColor(feature.geometry.coordinates[2]),
+        color: 'black',
+        fillOpacity: 0.75,
+        stroke: false,
+    })
+  }
+  });
+  // Sending our earthquakes layer to the createMap function
+  createMap(earthquakes);
+// }
 
 
 function createMap(earthquakes) {
-  
+
   // Create the tile layer that will be the background of the map
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -24,31 +53,29 @@ function createMap(earthquakes) {
   });
 
   var satellitemap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    maxZoom: 18,
-    id: "satellite-v9",
-    accessToken: API_KEY
-  })
+      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+      maxZoom: 18,
+      id: "satellite-v9",
+      accessToken: API_KEY
+  });
 
-  // Create a baseMaps object to hold the lightmap layer
   var baseMaps = {
-    "Light Map": lightmap,
-    "Satellite": satellitemap
+    "Satellite Map": satellitemap,
+    "Light Map": lightmap
   };
 
-  // Create an overlayMaps object to hold the earthquakes layer
   var overlayMaps = {
-    "Earthquakes": earthquakes
+    Earthquakes: earthquakes
   }
 
-  // Create the map object with options
   var myMap = L.map("mapid", {
     center: [35.546, -77.0525],
     zoom: 3,
-    layers: [lightmap, earthquakes]
+    layers: [satellitemap, earthquakes]
   });
 
-  // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
     collapsed: false
   }).addTo(myMap);
+}
+}
